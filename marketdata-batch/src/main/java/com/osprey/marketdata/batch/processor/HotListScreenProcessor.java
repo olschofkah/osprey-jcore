88cad95b178e46e8ec5930769872f3ba78d16a47
goliaeth <henry.olschofka@gmail.com>
2016-11-29 22:41:58 -0500

    Fixing NPE on missing fundamental data for a security

--- marketdata-batch/src/main/java/com/osprey/marketdata/batch/processor/HotListScreenProcessor.java
+++ marketdata-batch/src/main/java/com/osprey/marketdata/batch/processor/HotListScreenProcessor.java
@@ -43,32 +43,36 @@ public class HotListScreenProcessor implements ItemProcessor<SecurityQuoteContai
 
 		Date today = Date.valueOf(LocalDate.now());
 		HotListItem result = null;
-		for (ScreenStrategyEntry entry : screenProvidor.getScreens()) {
+		if (item.getFundamentalQuote() != null) {
+			for (ScreenStrategyEntry entry : screenProvidor.getScreens()) {
 
-			// Convert the criteria generators into criteria.
-			List<IScreenCriteria> criteria = new ArrayList<>(entry.getScreenCriteria().size());
-			for (ScreenCriteriaGenerator generator : entry.getScreenCriteria()) {
-				criteria.add(generator.generate());
-			}
-
-			criteria.addAll(initialScreenService.getCriteria());
-
-			SimpleScreenExecutor executor = new SimpleScreenExecutor();
-			executor.setPlans(screenPlanFactory.build(criteria));
-			executor.execute();
-
-			if (executor.getResultSet().contains(item.getKey())) {
-
-				logger.info("Adding {} to the hot list for {} ",
-						new Object[] { item.getKey().getSymbol(), LocalDate.now() });
-
-				if (result == null) {
-					result = new HotListItem(item.getKey());
+				// Convert the criteria generators into criteria.
+				List<IScreenCriteria> criteria = new ArrayList<>(entry.getScreenCriteria().size());
+				for (ScreenCriteriaGenerator generator : entry.getScreenCriteria()) {
+					criteria.add(generator.generate());
 				}
-				result.addModel(entry.getScreenName());
-				result.addAllStrategies(entry.getStrategies());
-				result.setReportDate(today);
+
+				criteria.addAll(initialScreenService.getCriteria());
+
+				SimpleScreenExecutor executor = new SimpleScreenExecutor();
+				executor.setPlans(screenPlanFactory.build(criteria));
+				executor.execute();
+
+				if (executor.getResultSet().contains(item.getKey())) {
+
+					logger.info("Adding {} to the hot list for {} ",
+							new Object[] { item.getKey().getSymbol(), LocalDate.now() });
+
+					if (result == null) {
+						result = new HotListItem(item.getKey());
+					}
+					result.addModel(entry.getScreenName());
+					result.addAllStrategies(entry.getStrategies());
+					result.setReportDate(today);
+				}
 			}
+		} else {
+			logger.warn("Skipping security {} due to missing fundamental data", () -> item.getKey().getSymbol());
 		}
 
 		return result;
